92a12d53cc27e03da4496ff01194165fcbfc934e
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-01-27 15:45:08 +0600

    back side navigate -1

--- src/Components/FriendDetails/FriendDetails.jsx
+++ src/Components/FriendDetails/FriendDetails.jsx
@@ -1,14 +1,20 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const FriendDetails = () => {
     const friend = useLoaderData();
     console.log(friend)
+
+    const navigate = useNavigate();
+    const handleBack = () => {
+        navigate(-1)
+    }
 
     return (
         <div>
             <h1>Name : {friend.name}</h1>
             <h2>Email : {friend.email}</h2>
             <h2>Web : {friend.website}</h2>
+            <button onClick={handleBack}>Go Back</button>
         </div>
     );
 };
